9a5f77aba069456e4265296ffb087e9ec5393753
Artur Cozzi <80040130+ArturCozzi@users.noreply.github.com>
2023-05-19 11:58:49 -0300

    criação dos itens do Menu e atualizar o status do item

---
+++ src/Menu/AtualizadorStatusItem.java
@@ -0,0 +1,21 @@
+package Menu;
+import javax.swing.*;
+
+import Gerenciador.GerenciadorMenu;
+
+public class AtualizadorStatusItem {
+    private GerenciadorMenu gerenciadorMenu;
+
+    public AtualizadorStatusItem(GerenciadorMenu gerenciadorMenu) {
+        this.gerenciadorMenu = gerenciadorMenu;
+    }
+
+    public void atualizarStatus() {
+        String nomeDoItem = JOptionPane.showInputDialog("Digite o nome do item que deseja atualizar o status:");
+        boolean novoStatus = JOptionPane.showConfirmDialog(null, "O item esta disponovel?", "Disponibilidade", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
+
+        if (!gerenciadorMenu.atualizarStatusItem(nomeDoItem, novoStatus)) {
+            JOptionPane.showMessageDialog(null, "Item nao encontrado. Tente novamente.");
+        }
+    }
+}
