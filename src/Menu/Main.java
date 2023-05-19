432be6c35146ebff2f051be557110b1e3d0ab2af
anacatarinop <anacatarinop@gmail.com>
2023-05-19 11:48:09 -0300

    Revert "criacao das classes"
    This reverts commit d01bee8ea350c05ed042a760a94acc9ec17607ce.

--- src/Menu/Main.java
+++
@@ -1,9 +0,0 @@
-package Menu;
-import Gerenciador.GerenciadorMenuGUI;
-
-public class Main {
-    public static void main(String[] args) {
-        GerenciadorMenuGUI gui = new GerenciadorMenuGUI();
-        gui.iniciar();
-    }
-}
