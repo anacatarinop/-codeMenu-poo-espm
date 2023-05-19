de0369eb8bb16e525beafbc792a8bfbfd650db5b
Artur Cozzi <80040130+ArturCozzi@users.noreply.github.com>
2023-05-19 12:43:29 -0300

    criar main e inicializar o menu

---
+++ src/Menu/Main.java
@@ -0,0 +1,9 @@
+package Menu;
+import Gerenciador.GerenciadorMenuGUI;
+
+public class Main {
+    public static void main(String[] args) {
+        GerenciadorMenuGUI gui = new GerenciadorMenuGUI();
+        gui.iniciar();
+    }
+}
