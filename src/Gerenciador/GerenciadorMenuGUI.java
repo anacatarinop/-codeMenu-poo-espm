4c777f3cc74ab97ec9ae35bc3b7e6d3f09fb54e1
Artur Cozzi <80040130+ArturCozzi@users.noreply.github.com>
2023-05-22 14:13:55 -0300

    Atualizar Menu do gerenciador
    colocar opcao de sair do menu e fazer as correcoes de dados inseridos erroneamente

--- src/Gerenciador/GerenciadorMenuGUI.java
+++ src/Gerenciador/GerenciadorMenuGUI.java
@@ -1,95 +1,98 @@
 package Gerenciador;
+
 import javax.swing.*;
 
-import Menu.AtualizadorStatusItem;
 import Menu.ItemMenu;
 
-import java.util.List;
 
 public class GerenciadorMenuGUI {
     private GerenciadorMenu gerenciadorMenu;
 
     public GerenciadorMenuGUI() {
-        gerenciadorMenu = new GerenciadorMenu();
+        this.gerenciadorMenu = new GerenciadorMenu();
     }
 
-    public void iniciar() {
+    public void display() {
         while (true) {
-            String escolha = JOptionPane.showInputDialog("Escolha uma opção:\n"
-                    + "1. Adicionar Item\n"
-                    + "2. Listar Itens\n"
-                    + "3. Atualizar Item\n"
-                    + "4. Remover Item\n"
-                    + "5. Atualizar Status do Item\n"
-                    + "Digite a opção:");
-
-            if (escolha == null) {  // se o usuário clicar em cancelar ou fechar a janela
-                break;
-            }
+            String escolha = JOptionPane.showInputDialog(null,
+                "Selecione uma opção:\n"
+                + "1: Adicionar Item\n"
+                + "2: Listar Itens\n"
+                + "3: Atualizar Item\n"
+                + "4: Remover Item\n"
+                + "5: Atualizar Status de um Item\n"
+                + "6: Sair\n",
+                "MENU",
+                JOptionPane.PLAIN_MESSAGE);
 
             switch (escolha) {
-           
-            case "1":
-                    String nome = JOptionPane.showInputDialog("Nome do Item:");
-                    String descricao = JOptionPane.showInputDialog("Descrição do Item:");
-                    double preco;
-                    try {
-                        preco = Double.parseDouble(JOptionPane.showInputDialog("Preço do Item:"));
-                    } catch (NumberFormatException e) {
-                        JOptionPane.showMessageDialog(null, "Preço inválido. Insira um número válido.");
-                        break;
-                    }
-                    boolean status = JOptionPane.showConfirmDialog(null, "Item está disponível?", "Disponibilidade", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
-
-                    ItemMenu novoItem = new ItemMenu(nome, descricao, preco, status);
-                    gerenciadorMenu.adicionarItem(novoItem);
-                    break;
-            
-            case "2":
-                    List<ItemMenu> itens = gerenciadorMenu.listarItens();
-                    if (itens.isEmpty()) {
-                        JOptionPane.showMessageDialog(null, "Não há itens para listar.");
-                        break;
-                    }
-                    StringBuilder itensStr = new StringBuilder();
-                    for (ItemMenu item : itens) {
-                        itensStr.append(item.toString()).append("\n");
-                    }
-                    JOptionPane.showMessageDialog(null, itensStr);
-                    break;
-      
-            case "3":                    
-            	String nomeParaAtualizar = JOptionPane.showInputDialog("Nome do Item para Atualizar:");
-                    if (gerenciadorMenu.listarItens().stream().noneMatch(item -> item.getNome().equals(nomeParaAtualizar))) {
-                        JOptionPane.showMessageDialog(null, "Item não encontrado. Tente novamente.");
-                        break;
-                    }
-                    String novaDescricao = JOptionPane.showInputDialog("Nova Descrição do Item:");
-                    double novoPreco;
-                    try {
-                        novoPreco = Double.parseDouble(JOptionPane.showInputDialog("Novo Preço do Item:"));
-                    } catch (NumberFormatException e) {
-                    	JOptionPane.showMessageDialog(null, "Preço inválido. Insira um número válido.");
-                        break;
-                    }
-                    boolean novoStatus = JOptionPane.showConfirmDialog(null, "Item está disponível?", "Disponibilidade", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
-
-                    if (!gerenciadorMenu.atualizarItem(nomeParaAtualizar, novaDescricao, novoPreco, novoStatus)) {
-                        JOptionPane.showMessageDialog(null, "Item não encontrado. Tente novamente.");
+            case "1": //Adicionar Item
+            	
+                    String codigo = JOptionPane.showInputDialog("Informe o código do item");
+                    if (!gerenciadorMenu.itemExiste(codigo)) {
+                        String nome = JOptionPane.showInputDialog("Informe o nome do item");
+                        String descricao = JOptionPane.showInputDialog("Informe a descrição do item");
+                        double preco = Double.parseDouble(JOptionPane.showInputDialog("Informe o preço do item"));
+                        boolean status = JOptionPane.showConfirmDialog(null, "O item está disponível?", "Disponibilidade", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
+                        ItemMenu item = new ItemMenu(codigo, nome, descricao, preco, status);
+                        gerenciadorMenu.adicionarItem(item);
+                        JOptionPane.showMessageDialog(null, "Item adicionado com sucesso!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Código já existente! Tente novamente com um código diferente.");
                     }
                     break;
-            case "4":
-                    String nomeParaRemover = JOptionPane.showInputDialog("Nome do Item para Remover:");
-                    if (!gerenciadorMenu.removerItem(nomeParaRemover)) {
-                        JOptionPane.showMessageDialog(null, "Item não encontrado. Tente novamente.");
-                    }
-            case "5":
-                AtualizadorStatusItem atualizador = new AtualizadorStatusItem(gerenciadorMenu);
-                atualizador.atualizarStatus();
+                
+            case "2": // Listar Itens
+                String itens = "";
+                for (ItemMenu i : gerenciadorMenu.listarItens()) {
+                    itens += "Código: " + i.getCodigo() + "\n" + i.toString() + "\n\n";
+                }
+                JOptionPane.showMessageDialog(null, itens);
                 break;
-                  
-            }
-            
+                
+                case "3": // Atualizar Item
+                    String codigoAtualizar = JOptionPane.showInputDialog("Informe o código do item que você deseja atualizar");
+                    if (gerenciadorMenu.itemExiste(codigoAtualizar)) {
+                        String novaDescricao = JOptionPane.showInputDialog("Informe a nova descrição do item");
+                        double novoPreco = Double.parseDouble(JOptionPane.showInputDialog("Informe o novo preço do item"));
+                        boolean novoStatus = JOptionPane.showConfirmDialog(null, "O item está disponível?", "Disponibilidade", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
+                        gerenciadorMenu.atualizarItem(codigoAtualizar, novaDescricao, novoPreco, novoStatus);
+                        JOptionPane.showMessageDialog(null, "Item atualizado com sucesso!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Código não encontrado! Tente novamente com um código existente.");
+                    }
+                    break;
+                
+                case "4": // Remover Item
+                    String codigoRemover = JOptionPane.showInputDialog("Informe o código do item que você deseja remover");
+                    if (gerenciadorMenu.itemExiste(codigoRemover)) {
+                        gerenciadorMenu.removerItem(codigoRemover);
+                        JOptionPane.showMessageDialog(null, "Item removido com sucesso!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Código não encontrado! Tente novamente com um código existente.");
+                    }
+                    break;
+                
+                case "5": // Atualizar Status de um Item
+                    String codigoStatus = JOptionPane.showInputDialog("Informe o código do item cujo status você deseja atualizar");
+                    if (gerenciadorMenu.itemExiste(codigoStatus)) {
+                        boolean statusAtualizar = JOptionPane.showConfirmDialog(null, "O item está disponível?", "Disponibilidade", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION;
+                        gerenciadorMenu.atualizarStatusItem(codigoStatus, statusAtualizar);
+                        JOptionPane.showMessageDialog(null, "Status do item atualizado com sucesso!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Código não encontrado! Tente novamente com um código existente.");
+                    }
+                    break;
+                
+                case "6": // Sair
+                    JOptionPane.showMessageDialog(null, "Menu fechado com sucesso.");
+                    System.exit(0);
+                    break;
+                 default:
+                     JOptionPane.showMessageDialog(null, "Opção inválida. Por favor, escolha um número entre 1 e 6.");
+                     break;
+             }
+         
         }
     }
 }
